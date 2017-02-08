066da60f033964fc4b7c4d6b3b74e250dbeaf118
Kevin Koh <jkoh@nd.edu>
2017-02-08 10:56:34 -0500

    Saving work for BO

--- tdscf/bo.py
+++ tdscf/bo.py
@@ -260,38 +260,48 @@ class BORKS():
         Make a BlockOrthogonalizedRHF
         Args: mol1_, mol2_ fragments to make a block orthogonal SCF from.
         """
-
+# to Be SORTED
         # bb ojbect(optional)
         self.BB = BB
         self.mb = None
         self.rksb = None
-        #mol objects
+
+
+# UP TO HERE
+
+        # objects
         self.m1, self.m2, self.m3 = self.MixedBasisMol(atom,basis,n)
-        # rks objects
         self.rks1, self.rks2, self.rks3 = self.MixedTheory(xc)
-        # TO BE DELETED later
+        # Global Variables
         self.AOExc = None
         self.BOExc = None
-        # Different Matrix
-        # U: |AO><BO|
-        self.U = BOprojector(self.m1,self.m3)
-        # O: |BO><MO|
-        self.O = None
-        self.Vtilde = None
-        self.Enuc = self.rks3.e_tot - dft.rks.energy_elec(self.rks3,self.rks3.make_rdm1())[0]
-        self.H = self.rks3.get_hcore()
-        self.Htilde = TransMat(self.H,self.U)
-        self.S = self.rks3.get_ovlp()
-        self.Stilde = TransMat(self.S,self.U)
-        # Constants
+        self.EBO = None
         self.nA = self.m1.nao_nr()
         self.n = self.m3.nao_nr()
-        # output
-        self.EBO = None
+        self.n_ao = None
+        self.n_mo = None
+        self.n_aux = None
+        self.n_occ = None
+        self.Enuc = self.rks3.e_tot - dft.rks.energy_elec(self.rks3,self.rks3.make_rdm1())[0]
+        self.hybl = self.rks3._numint.hybrid_coeff(self.rks3.xc, spin=(self.rks3.mol.spin>0)+1)
+        self.hybh = self.rks3._numint.hybrid_coeff(self.rks1.xc, spin=(self.rks1.mol.spin>0)+1)
+        # Global Matrices
+        self.H = self.rks3.get_hcore()
+        self.S = self.rks3.get_ovlp() # AOxAO
+        self.U = BOprojector(self.m1,self.m3) #(AO->BO)
+        self.Htilde = TransMat(self.H,self.U)
+        self.Stilde = TransMat(self.S,self.U)
+        self.X = MatrixPower(self.S,-1./2.) # AO->LAO
+        self.O = None #(MO->BO) = C * U
+        self.Vtilde = None
+        # Propagation Steps
+        self.auxmol_set()
+        self.initialcondition()
 
 
-        self.Ptilde = self.GetGuess()
-        self.Ftilde = self.FockGuess()
+
+        #self.Ptilde = self.GetGuess()
+        #self.Ftilde = self.FockGuess()
 
     def MixedBasisMol(self, atm, bas, n):
         # For mixed Basis of AA and BB
@@ -348,15 +358,15 @@ class BORKS():
         # Generate Mixed XC dft objects
         m1 = dft.rks.RKS(self.m1)
         m1.xc = xc[0]
-        print "High Theory AA"
+        print "High Theory AA:",xc[0]
         m1.kernel()
         m2 = dft.rks.RKS(self.m2)
         m2.xc = xc[1]
-        print "Low Theory AA"
+        print "Low Theory AA:",xc[1]
         m2.kernel()
         m3 = dft.rks.RKS(self.m3)
         m3.xc = xc[1]
-        print "Low Theory"
+        print "Low Theory:",xc[1]
         m3.kernel()
         # optional BB
         if self.BB != 0:
@@ -366,6 +376,252 @@ class BORKS():
             rksb.kernel()
             self.rksb = rksb
         return m1,m2,m3
+
+    def initialcondition(self):
+        print '''
+        Condition setup
+        Read Param
+        InitializeLiouvillian
+        '''
+        n_ao = self.n_ao = self.rks3.make_rdm1().shape[0]
+        n_mo = self.n_mo = n_ao # should be fixed.
+        n_occ = self.n_occ = int(sum(self.rks3.mo_occ)/2)
+        print "n_ao:", n_ao, "n_mo:", n_mo, "n_occ:", n_occ
+        print self.nA, self.n
+        self.ReadParams()
+        self.InitializeLiouvillian()
+        return
+
+    def ReadParams(self):
+        print '''
+        read Parameters
+        If there is any param
+        '''
+        return
+
+    def InitializeLiouvillian(self):
+        self.InitFockBuild()
+
+
+
+    def InitFockBuild(self):
+        '''
+        Using Roothan's equation to build a Fock matrix and initial density matrix
+        Returns:
+            self consistent density in Block Orthogonalized basis.
+        '''
+        n_occ = self.n_occ
+        Ne = self.n_e = 2.0 * n_occ
+        err = 100
+        it = 0
+        S = self.S.copy()
+        Pbo = TransMat(self.rks3.make_rdm1(), self.U)
+        self.F = self.BODFT_Fockbuild(Pbo)
+        Pbo_old = Pbo.copy()
+        E = self.energy(Pbo)
+        print "Initial Energy:",E
+        while (err > 10**-10):
+            # Diagonalize F in the BO basis
+            self.eigs, self.V = np.linalg.eig(self.F)
+            idx = self.eigs.argsort()
+            self.eigs.sort()
+            self.V = self.V[:,idx].copy()
+            # Fill up the density in the MO basis and then Transform back
+            print "NE(Lit):", TrDot(Pbo,self.Stilde)
+            Pmo = 0.5*np.diag(self.rks3.mo_occ).astype(complex)
+            Pbo = TransMat(Pmo,self.V,-1) # V = (BO -> MO)
+            print "Ne", np.trace(Pbo), np.trace(Pmo)
+            Eold = E
+            self.F = self.BODFT_Fockbuild(Pbo)
+            E = self.energy(Pbo)
+            err = abs(E-Eold)
+            if (it%1 ==0):
+                print "Iteration:", it,"; Energy:",E,"; Error =",err
+            it += 1
+        quit()
+        Pmo = 0.5*np.diag(self.the_scf.mo_occ).astype(complex)
+        Plao = TransMat(Pmo,self.V,-1)
+        self.C = np.dot(self.X,self.V)
+        self.rho = TransMat(Plao,self.V)
+        self.rhoM12 = TransMat(Plao,self.V)
+        print "Ne", np.trace(Plao), np.trace(self.rho),
+        print "Energy:",E
+        print "Initial Fock Matrix(MO)\n", TransMat(self.F,self.V)
+        return Plao
+
+    def BODFT_Fockbuild(self,P):
+        """
+        Updates self.F given current self.rho (both complex.)
+        Fock matrix with HF
+        Args:
+            P = BO density matrix.
+        Returns:
+            Fock matrix(BO) . Updates self.F
+        """
+        Pt = 2.0*TransMat(P,self.U,-1) # to AO
+        J = self.get_j(Pt)
+        Vlow = self.get_vxc(Pt) # Include the Hybrid with K matrix
+        Veff = J + Vlow # Whole J - 0.5K
+        nA = self.nA
+        n = self.n
+        # pass down AA block of BO density matrix
+        VAAhigh = np.zeros((n,n)).astype(complex)
+        VAAlow = np.zeros((n,n)).astype(complex)
+        VAAhigh[:nA,:nA] = self.get_vxchigh(2*P[:nA,:nA])
+        VAAlow[:nA,:nA] = self.get_vxclow(2*P[:nA,:nA])
+        VAA = VAAhigh - VAAlow # already in BO basis
+        self.F = TransMat(self.H + 0.5*(Veff + Veff.T.conj()),self.U) #+ VAA
+        return self.F
+
+    def energy(self,Pbo,IfPrint=False):
+        """
+        P: Density in BO basis.
+        """
+        Hbo = self.Htilde
+        TrDot(Pbo,self.H)
+
+        P = TransMat(Pbo,self.U,-1)
+        J = self.get_j(2*P)
+        nn, Exc, Vx = self.rks3._numint.nr_rks(self.rks3.mol, self.rks3.grids, self.rks3.xc, 2*P.real, 0)
+        Exc -= 0.5 * 0.5 * self.hybl * TrDot(P,self.get_k(2*P))
+        #nn, Excl, Vx = self.rks2._numint.nr_rks(self.rks2.mol, self.rks2.grids, self.rks2.xc, 2*P.real, 0)
+        #need hybrid as well
+        #nn, Exch, Vx = self.rks1._numint.nr_rks(self.rks1.mol, self.rks1.grids, self.rks1.xc, 2*P.real, 0)
+        EH = TrDot(Pbo,2*Hbo)
+        EJ = TrDot(P,J)
+        E = EH + EJ + Exc #+ (Exch - Excl)
+        E += self.Enuc
+        return E.real
+
+    def get_jk(self, P):
+        '''
+        Args:
+            P: AO density matrix
+        Returns:
+            J: Coulomb matrix
+            K: Exchange matrix
+        '''
+        return self.get_j(P), self.get_k(P)
+
+    def get_j(self,P):
+        '''
+        Args:
+            P: AO density matrix
+
+        Returns:
+            J: Coulomb matrix (AO)
+        '''
+        naux = self.n_aux
+        nao = self.n_ao
+        rho = np.einsum('ijp,ij->p', self.eri3c, P)
+        rho = np.linalg.solve(self.eri2c, rho)
+        jmat = np.einsum('p,ijp->ij', rho, self.eri3c)
+        return jmat
+
+    def get_k(self,P):
+        '''
+        Args:
+            P: AO density matrix
+        Returns:
+            K: Exchange matrix (AO)
+        '''
+        naux = self.n_aux
+        nao = self.n_ao
+        kpj = np.einsum('ijp,jk->ikp', self.eri3c, P)
+        pik = np.linalg.solve(self.eri2c, kpj.reshape(-1,naux).T.conj())
+        rkmat = np.einsum('pik,kjp->ij', pik.reshape(naux,nao,nao), self.eri3c)
+        return rkmat
+
+    def get_vxc(self,P):
+        '''
+        Args:
+            P: AO density matrix
+
+        Returns:
+            Vxc: Exchange and Correlation matrix (AO)
+        '''
+        K = self.get_k(P)
+        nn, Exc, Vxc = self.rks3._numint.nr_rks(self.rks3.mol, self.rks3.grids, self.rks3.xc, P.real, 0)
+        Vxc = Vxc.astype(complex)
+        Vxc += -0.5 * self.hybl * K
+        return Vxc
+
+    def get_vxclow(self,P):
+        '''
+        Assume no hybrid functionals for now (due to K built using rks3 size)
+        Args:
+            P: BO density matrix
+        Returns:
+            Vxc: Exchange and Correlation matrix (AO)
+        '''
+        #K = self.get_k(P)
+        nn, Exc, Vxc = self.rks2._numint.nr_rks(self.rks2.mol, self.rks2.grids, self.rks2.xc, P.real, 0)
+        Vxc = Vxc.astype(complex)
+        #Vxc += -0.5 * self.hybl * K
+        return Vxc
+
+    def get_vxchigh(self,P):
+        '''
+        Assume no hybrid functionals for now
+        Args:
+            P: BO density matrix
+
+        Returns:
+            Vxc: Exchange and Correlation matrix (AO)
+        '''
+        #K = self.get_k(P)
+        nn, Exc, Vxc = self.rks1._numint.nr_rks(self.rks1.mol, self.rks1.grids, self.rks1.xc, P.real, 0)
+        Vxc = Vxc.astype(complex)
+        #Vxc += -0.5 * self.hybh * K
+        return Vxc
+
+    def auxmol_set(self,auxbas = "weigend"):
+        auxmol = gto.Mole()
+        auxmol.atom = self.rks3.mol.atom
+        auxmol.basis = auxbas
+        auxmol.build()
+        mol = self.rks3.mol
+        nao = self.n_ao = mol.nao_nr()
+        naux = self.n_aux = auxmol.nao_nr()
+        #print nao
+        #print naux
+        atm, bas, env = gto.conc_env(mol._atm, mol._bas, mol._env, auxmol._atm, auxmol._bas, auxmol._env)
+        eri3c = np.empty((nao,nao,naux))
+        pi = 0
+        for i in range(mol.nbas):
+            pj = 0
+            for j in range(mol.nbas):
+                pk = 0
+                for k in range(mol.nbas, mol.nbas+auxmol.nbas):
+                    shls = (i, j, k)
+                    buf = gto.getints_by_shell('cint3c2e_sph', shls, atm, bas, env)
+                    di, dj, dk = buf.shape
+                    eri3c[pi:pi+di,pj:pj+dj,pk:pk+dk] = buf
+                    pk += dk
+                pj += dj
+            pi += di
+
+        eri2c = np.empty((naux,naux))
+        pk = 0
+        for k in range(mol.nbas, mol.nbas+auxmol.nbas):
+            pl = 0
+            for l in range(mol.nbas, mol.nbas+auxmol.nbas):
+                shls = (k, l)
+                buf = gto.getints_by_shell('cint2c2e_sph', shls, atm, bas, env)
+                dk, dl = buf.shape
+                eri2c[pk:pk+dk,pl:pl+dl] = buf
+                pl += dl
+            pk += dk
+
+        self.eri3c = eri3c
+        self.eri2c = eri2c
+
+        RSinv = MatrixPower(eri2c,-0.5)
+        self.B = np.einsum('ijp,pq->ijq', self.eri3c, RSinv) # (AO,AO,n_aux)
+
+        return
+
+
 
     def FockGuess(self):
         print "\n\n======================================"
